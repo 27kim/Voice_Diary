ed8d4b114996c5a9d58813d4acbd3eb297ae8057
27kim <doohyeonkim@gmail.com>
2018-09-23 21:29:38 +0900

    Initial Commit

---
+++ Voice_Diary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Voice_Diary
+//
+//  Created by 27k on 23/09/2018.
+//  Copyright © 2018 27k. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Voice_DiaryTests/Voice_DiaryTests.swift
@@ -0,0 +1,34 @@
+//
+//  Voice_DiaryTests.swift
+//  Voice_DiaryTests
+//
+//  Created by 27k on 23/09/2018.
+//  Copyright © 2018 27k. All rights reserved.
+//
+
+import XCTest
+@testable import Voice_Diary
+
+class Voice_DiaryTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
