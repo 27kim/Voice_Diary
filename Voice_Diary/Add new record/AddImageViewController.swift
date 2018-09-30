71eb3c781b612af470243712d44fb8f5ab5d3838
27kim <doohyeonkim@gmail.com>
2018-09-30 12:11:26 +0900

    Firebase 붙임

--- Voice_Diary/Add new record/AddImageViewController.swift
+++ Voice_Diary/Add new record/AddImageViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-//import FirebaseStorage
 import Photos
 import Firebase
 //import FirebaseDatabase
@@ -24,7 +23,7 @@ class AddImageViewController: UIViewController, UIImagePickerControllerDelegate,
     @IBOutlet var indicator: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
-       print("Title \(PTitle) and Desc \(PDesc)" )
+        print("Title \(PTitle) and Desc \(PDesc)" )
         imagePicker.delegate = self
         
         
@@ -33,9 +32,9 @@ class AddImageViewController: UIViewController, UIImagePickerControllerDelegate,
                 #selector(DidTapAdd(_:))
             )
         )
-
-
-
+        
+        
+        
     }
     @objc func DidTapAdd(_ sender: Any) {
         
@@ -46,61 +45,95 @@ class AddImageViewController: UIViewController, UIImagePickerControllerDelegate,
         
         
         
-
+        
         
     }
-    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-         self.indicator.startAnimating()
-         self.goButton.alpha = 0.5
-         self.goButton.isEnabled = false
-         self.PostImage.alpha = 0.5
-         self.PostImage.isUserInteractionEnabled = false
-//
-//        if let pickedImage = info[.originalImage] as! UIImage {
-//            PostImage.contentMode = .scaleAspectFit
-//            let storage = Storage.storage()
-//
-//
-//
-//
-//            // Create a storage reference from our storage service
-//            let storageRef = storage.reference()
-//            let  data = UIImageJPEGRepresentation(pickedImage, 1)
-//            let randomName = randomStringWithLength(len: 15)
-//            // Create a reference to the file you want to upload
-//            let riversRef = storageRef.child("images/\(randomName).jpg")
-//
-//            // Upload the file to the path "images/rivers.jpg"
-//            _ = riversRef.putData(data!, metadata: nil) { (metadata, error) in
-//                guard let metadata = metadata else {
-//                    // Uh-oh, an error occurred!
-//                    self.PostImage.animation = "shake"
-//                    self.PostImage.duration = 1
-//                    self.PostImage.animate()
-//                    return
-//                }
-//
-//
-//
-//                let downloadURL = metadata.downloadURL()
-//               // print("Download image \(String(describing: downloadURL))")
-//                let dl = String(describing: downloadURL!)
-//                self.PDL = dl
-//                print(dl)
-//
-//
-//                self.PostImage.contentMode = .scaleToFill
-//                self.PostImage.image = pickedImage
-//
-//                self.indicator.stopAnimating()
-//                self.goButton.alpha = 1
-//                self.PostImage.alpha = 1
-//                self.PostImage.isUserInteractionEnabled = true
-//                self.goButton.isEnabled = true
-//
-//            }
-//
-//        }
+    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        self.indicator.startAnimating()
+        self.goButton.alpha = 0.5
+        self.goButton.isEnabled = false
+        self.PostImage.alpha = 0.5
+        self.PostImage.isUserInteractionEnabled = false
+        
+        if let pickedImage = info[.originalImage] as? UIImage {
+            PostImage.contentMode = .scaleAspectFit
+            let storage = Storage.storage()
+            
+            
+            // Create a storage reference from our storage service
+            let storageRef = storage.reference()
+            let  data = pickedImage.jpegData(compressionQuality: 1)
+            //            let data = UIImage.jpegData(pickedImage)
+            let randomName = randomStringWithLength(len: 15)
+            // Create a reference to the file you want to upload
+            let riversRef = storageRef.child("images/\(randomName).jpg")
+            
+            // Upload the file to the path "images/rivers.jpg"
+            let uploadTask = riversRef.putData(data!, metadata: nil) { (metadata, error) in
+                guard let metadata = metadata else {
+                    // Uh-oh, an error occurred!
+                    self.PostImage.animation = "shake"
+                    self.PostImage.duration = 1
+                    self.PostImage.animate()
+                    return
+                }
+                // Metadata contains file metadata such as size, content-type.
+                let size = metadata.size
+                // You can also access to download URL after upload.
+                storageRef.downloadURL { (url, error) in
+                    guard let downloadURL = url else {
+                        // Uh-oh, an error occurred!
+                        return
+                    }
+                    
+                    let dl = String(describing: url!)
+                    self.PDL = dl
+                    print(dl)
+                    
+                    
+                    self.PostImage.contentMode = .scaleToFill
+                    self.PostImage.image = pickedImage
+                    
+                    self.indicator.stopAnimating()
+                    self.goButton.alpha = 1
+                    self.PostImage.alpha = 1
+                    self.PostImage.isUserInteractionEnabled = true
+                    self.goButton.isEnabled = true
+                }
+                
+            }
+            
+            // Upload the file to the path "images/rivers.jpg"
+            //            _ = riversRef.putData((data)!, metadata: nil) { (metadata, error) in
+            //                guard let metadata = metadata else {
+            //                    // Uh-oh, an error occurred!
+            //                    self.PostImage.animation = "shake"
+            //                    self.PostImage.duration = 1
+            //                    self.PostImage.animate()
+            //                    return
+            //                }
+            //
+            //
+            //
+            //                let downloadURL = metadata.downloadURL()
+            //               // print("Download image \(String(describing: downloadURL))")
+            //                let dl = String(describing: downloadURL!)
+            //                self.PDL = dl
+            //                print(dl)
+            //
+            //
+            //                self.PostImage.contentMode = .scaleToFill
+            //                self.PostImage.image = pickedImage
+            //
+            //                self.indicator.stopAnimating()
+            //                self.goButton.alpha = 1
+            //                self.PostImage.alpha = 1
+            //                self.PostImage.isUserInteractionEnabled = true
+            //                self.goButton.isEnabled = true
+            //
+            //            }
+            
+        }
         
         dismiss(animated: true, completion: nil)
     }
@@ -113,28 +146,28 @@ class AddImageViewController: UIViewController, UIImagePickerControllerDelegate,
         } else {
             
         }
-//         self.SaveToFirebase(downloadURL: PDL, Title: self.PTitle, Desc: self.PDesc)
+        self.SaveToFirebase(downloadURL: PDL, Title: self.PTitle, Desc: self.PDesc)
         self.navigationController?.popToRootViewController(animated: true)
         
         
     }
     
     func SaveToFirebase(downloadURL: String,Title:String,Desc:String) {
-//        let date = NSDate()
-//        let dateFormatter = DateFormatter()
-//        dateFormatter.dateFormat  = "EEEE, MMMM dd, yyyy, h:mm:ss"//"EE" to get short style
-//        let dayInWeek = dateFormatter.string(from: date as Date)//"Sunday"
-//        var ref: DatabaseReference!
-//        let user = Auth.auth().currentUser
-//        ref = Database.database().reference()
-//        let post:[String : Any] = [
-//            "Title":Title,
-//            "Desc":Desc,
-//            "Picture": downloadURL,
-//            "Date": dayInWeek,
-//            "isFav":"false",
-//            ]
-//         ref.child("Data").child(user!.uid).child("Data").childByAutoId().setValue(post)
+        let date = NSDate()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat  = "EEEE, MMMM dd, yyyy, h:mm:ss"//"EE" to get short style
+        let dayInWeek = dateFormatter.string(from: date as Date)//"Sunday"
+        var ref: DatabaseReference!
+        let user = Auth.auth().currentUser
+        ref = Database.database().reference()
+        let post:[String : Any] = [
+            "Title":Title,
+            "Desc":Desc,
+            "Picture": downloadURL,
+            "Date": dayInWeek,
+            "isFav":"false",
+            ]
+        ref.child("Data").child(user!.uid).child("Data").childByAutoId().setValue(post)
         
         
     }
